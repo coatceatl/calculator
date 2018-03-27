bfa143ad51f9541690443cbc1c1aa40687a7ca82
Oksana Tkachenko <oksana@crystax.net>
2018-03-28 01:17:01 +0300

    adds jquery

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+window.onload = function () {
+    document.getElementById('#lets_go').innerHTML = 'Lemme Go!';
+};
+*/
+
+$(document).ready(function() {
+    $('#lets_go').text('Lemme Go!');
+});
